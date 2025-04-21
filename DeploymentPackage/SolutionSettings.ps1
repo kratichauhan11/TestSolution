e20bd742523878de303c1f73e1625fdce4626b94
Neeraj Jangid <neeraj.j@intimetec.com>
2025-04-21 14:58:05 +0530

    Initial commit

---
+++ DeploymentPackage/SolutionSettings.ps1
@@ -0,0 +1,6 @@
+﻿@{
+	CdsSolutionName = "TestSolution"
+	PackageDllFile="DeploymentPackage.dll"
+	PackageFolder="DeploymentPackage"
+	CdsSolutionDependencies = "" # Ordered list of dependency solution file names.
+}
